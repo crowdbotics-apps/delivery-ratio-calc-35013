0a87240b628a85b0dc1740ef7f07c767fcb2955f
Crowdbotics <team@crowdbotics.com>
2022-04-30 05:14:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "delivery_ratio_calc_35013",
+  url: "https://delivery_ratio_calc_35013.botics.co",
+  api: "https://delivery_ratio_calc_35013.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
